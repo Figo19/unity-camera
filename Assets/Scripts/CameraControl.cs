97dbf39dd34fc6aec0f5349f316823ab932f05a5
Figo <26819403+Figo19@users.noreply.github.com>
2019-03-24 23:50:11 +0000

    Added all the related script files
    Initial commit with current scripts for the project.
    This includes a 1st and 3rd person mode camera

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public enum CameraState { FirstPerson, ThirdPerson};
+    public CameraState cameraState = CameraState.FirstPerson;
+    Camera playerCam;
+    PlayerControl playerControl;
+    float cameraDistance;
+    public float zoomSpeed = 125f;
+    public Vector3 cameraOffset = new Vector3(0.75f, 1.25f, 0f);
+
+
+    public void Start () {
+        playerCam = GetComponentInChildren<Camera>();
+        playerControl = GetComponent<PlayerControl>();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;        
+        UpdateCameraPosition();
+        cameraDistance = (cameraState == CameraState.ThirdPerson) ? 1 : 0;
+    }
+
+
+    public void LateUpdate() {
+
+        cameraDistance = Mathf.Clamp(cameraDistance - Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime * zoomSpeed, 0f, 2f);
+        if (cameraDistance <= 0.8) {
+            cameraState = CameraState.FirstPerson;
+        } else {
+            cameraState = CameraState.ThirdPerson;
+        }
+
+        UpdateCameraPosition();
+    }
+
+
+    public void Update() {
+        if (Input.GetKey(KeyCode.Escape)) {
+            if (Cursor.lockState == CursorLockMode.Locked) {
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+    }
+
+
+    void UpdateCameraPosition() {
+
+        switch(cameraState) {
+            case CameraState.FirstPerson:
+                FirstPersonCameraUpdate(playerCam);
+                break;
+
+            case CameraState.ThirdPerson:
+                ThirdPersonCameraUpdate(playerCam);
+                break;
+
+            default:
+                Debug.Log("Undefined Camera State!");
+                break;
+        }
+
+
+    }
+
+
+    void FirstPersonCameraUpdate(Camera playerCamera) {
+
+        MeshRenderer playerMesh =  GetComponent<MeshRenderer>();
+        if (playerMesh.enabled) {
+            playerMesh.enabled = false;
+            foreach (MeshRenderer mesh in GetComponentsInChildren<MeshRenderer>()) {
+                mesh.enabled = false;
+            }
+        }
+
+        playerCam.transform.localPosition = Vector3.zero;
+        playerCam.transform.Rotate(Input.GetAxisRaw("Mouse Y") * playerControl.rotationSpeed * Vector3.left * Time.deltaTime);
+
+        float xEulerAngle = AngleClamp(playerCam.transform.localEulerAngles.x, -75f, 75f);
+        playerCam.transform.localEulerAngles = new Vector3(xEulerAngle, 0f, 0f);
+    }
+
+
+    void ThirdPersonCameraUpdate(Camera playerCamera) {
+
+        MeshRenderer playerMesh = GetComponent<MeshRenderer>();
+        if (!playerMesh.enabled) {
+            playerMesh.enabled = true;
+            foreach (MeshRenderer mesh in GetComponentsInChildren<MeshRenderer>()) {
+                mesh.enabled = true;
+            }
+        }
+        
+        // Vertical rotation
+        playerCam.transform.Rotate(Input.GetAxisRaw("Mouse Y") * playerControl.rotationSpeed * Vector3.left * Time.deltaTime);
+
+        // Clamping angles
+        float xEulerAngle = AngleClamp(playerCam.transform.localEulerAngles.x, -75f, 75f);
+        playerCam.transform.localEulerAngles = new Vector3(xEulerAngle, 0f, 0f);
+
+        // Update Position
+        Vector3 playerCameraPosition = new Vector3(0f, Mathf.Sin(xEulerAngle * Mathf.PI / 180f), -Mathf.Cos(xEulerAngle * Mathf.PI / 180f)) * cameraDistance * 3f + cameraOffset;
+        playerCam.transform.localPosition = playerCameraPosition;
+
+        //Check for colisions
+        //Debug.DrawRay(transform.position, playerCam.transform.position - transform.position, Color.red);
+        if (Physics.Linecast(transform.position, playerCam.transform.position, out RaycastHit hitInfo)) {
+            //Debug.Log(hitInfo.distance);
+            playerCam.transform.localPosition = playerCameraPosition.normalized * Mathf.Max(hitInfo.distance, 1.5f);
+        }
+
+    }
+
+
+    float AngleClamp(float angle, float min, float max) {
+        return Mathf.Clamp(angle > 180f ? angle - 360f : angle, min, max);
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    
+    public float movementSpeed = 1f;
+    public float rotationSpeed = 1f;
+    Rigidbody rb;
+
+    public void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    public void FixedUpdate() {
+
+        Vector3 movementVelocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")).normalized * movementSpeed;
+        rb.transform.Translate(movementVelocity * Time.fixedDeltaTime);
+
+        rb.transform.Rotate(Input.GetAxisRaw("Mouse X") * Vector3.up * rotationSpeed * Time.fixedDeltaTime);
+    }
+
+
+
+
+    public void OnValidate() {
+        movementSpeed = Mathf.Max(movementSpeed, 0f);
+        rotationSpeed = Mathf.Max(rotationSpeed, 0f);
+
+    }
+}
